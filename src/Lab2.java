0ac8be62d78c449f6c6eb0b2420da8a60b34ac55
Eugene <evgeniy0ctreles@gmail.com>
2020-03-03 18:44:54 +0700

    Lab 2 is done!

--- src/Lab2.java
+++ src/Lab2.java
@@ -1,2 +1,18 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
 public class Lab2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Hello!\nThis program calculate area of triangle\n\nEnter the A: ");
+        double a = Double.parseDouble(scan.nextLine());
+        System.out.print("Okay! Now enter the H of your shape: ");
+        double h = Double.parseDouble(scan.nextLine());
+        System.out.print("\nHere we go!\n" +
+                "A = " + a+
+                "\nH = " + h + "\n");
+        System.out.print("\nWell done! Here's yours result -- S= 0,5АН = " + 0.5*a*h +
+                ".\n---Good Luck!---\n\n");
+
+    }
 }
